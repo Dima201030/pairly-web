7c71bbeca92c738a78d7780b56be368d279a5193
lilnimp <dima@kojem.ru>
2026-08-10 04:27:19 +0300

    Force dynamic for login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import { useState } from 'react';
 import { useAuth } from '@/lib/AuthContext';
 import { useToast } from '@/components/ui/Toast';
