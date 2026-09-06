584a87523b8a5f890a3c6dfbbc1accd2deaee1c1
Дмитрий <dima@MacBook-Pro-Dmitrij.local>
2026-09-06 03:07:02 +0300

    fix: use cert(filePath) for firebase-admin

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,26 +1,23 @@
 import { initializeApp, getApps, cert, type App } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
 
 let app: App;
 
 if (!getApps().length) {
-  let serviceAccount: Record<string, string>;
-
   const keyPath = process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_PATH;
   if (keyPath) {
-    serviceAccount = JSON.parse(readFileSync(resolve(keyPath), 'utf-8'));
+    app = initializeApp({
+      credential: cert(keyPath),
+    });
   } else {
-    serviceAccount = JSON.parse(
+    const serviceAccount = JSON.parse(
       process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT!
     );
+    app = initializeApp({
+      credential: cert(serviceAccount as any),
+    });
   }
-
-  app = initializeApp({
-    credential: cert(serviceAccount as any),
-  });
 } else {
   app = getApps()[0];
 }
